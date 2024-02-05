55b7accfcc2757f85a85c00c8c1b00e30ecf231f
Denys Bondartsov <zimoby@gmail.com>
2024-02-05 05:24:29 +0200

    fix relative distance

--- src/utils/calculations.tsx
+++ src/utils/calculations.tsx
@@ -1,9 +1,12 @@
 export const calculateRelativeDistance = (distance, sliderValue) => {
-  return Math.pow(distance, 1 / sliderValue);
+  const offset = 0.75;
+  return Math.pow((distance + offset), 1 / sliderValue) - offset;
 };
 
 export const calculateRelativeScale = (size, sliderValue) => {
-  return Math.pow(size, 1 / sliderValue) / sliderValue - (sliderValue - 1) * 0.007;
+  const offset = 0.75;
+  return Math.pow(size, 1 / sliderValue) / sliderValue;
+  // return Math.pow(size, 1 / sliderValue) / sliderValue - (sliderValue - 1) * 0.007;
 };
 
 export const degreesToRadians = (degrees) => (degrees * Math.PI) / 180;
