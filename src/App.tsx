db65cafb678671eaddb1bef474a8eaab5c35f2da
Denys Bondartsov <zimoby@gmail.com>
2024-02-12 20:20:57 +0200

    add info

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ import { useSyncControlsWithStore } from "./hooks/controls";
 import { KeyboardControls, KeyboardControlsEntry } from "@react-three/drei";
 import { Controls } from "./types";
 import { useSystemStore } from "./store/systemStore";
+import { SolarEffects } from "./components/Scene/effects";
 
 function App() {
-  const { isInitialized, isInitialized2 } = useSystemStore((state) => state);
   useSyncControlsWithStore();
 
   const cameraDistance = 100;
@@ -25,20 +25,30 @@ function App() {
     ], []
   );
 
-
-
   return (
-    <KeyboardControls map={map}>
-      <Canvas gl={{ alpha: false }} camera={{ near: 0.01, far: 500, fov: 10, position: [0, 100, 100 * 2] }}>
-        <color args={["#111111"]} attach="background" />
-        
-        <AppStatsPerformance />
-        <SceneSetup />
-        <SolarSystem />
-        <ControlComponent />
-        <KeyboardInit />
-      </Canvas>
-    </KeyboardControls>
+    <>
+      <KeyboardControls map={map}>
+        <Canvas gl={{ alpha: false }} camera={{ fov: 10, position: [0, cameraDistance, cameraDistance * 2] }}>
+          <color args={["#111111"]} attach="background" />
+          
+          {/* <SolarEffects /> */}
+          <AppStatsPerformance />
+          <SceneSetup />
+          <SolarSystem />
+          <ControlComponent />
+          <KeyboardInit />
+        </Canvas>
+      </KeyboardControls>
+      <div className="absolute bottom-0 left-1/2">
+        <div className="flex flex-col">
+            <div className="flex flex-row space-x-3 opacity-50  p-2">
+            <p className="text-white text-xs">Three.js Journey</p>
+            <p className="text-white text-xs">Challenge 008: Solar System</p>
+            <p className="text-white text-xs">Author: Bondartsov Denys</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
