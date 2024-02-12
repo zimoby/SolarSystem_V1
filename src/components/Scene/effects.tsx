71261dd6d34010557b214b1bff34c16f7b9de1a1
Denys Bondartsov <zimoby@gmail.com>
2024-02-12 20:22:20 +0200

    add effects as component

---
+++ src/components/Scene/effects.tsx
@@ -0,0 +1,16 @@
+import { Effects } from "@react-three/drei"
+import { extend } from "@react-three/fiber"
+import { FilmPass, WaterPass, UnrealBloomPass, LUTPass, LUTCubeLoader } from 'three-stdlib'
+
+extend({ WaterPass, UnrealBloomPass, FilmPass, LUTPass })
+
+export const SolarEffects = () => {
+
+    return (
+        <Effects>
+            <unrealBloomPass args={[undefined, 1.25, 1, 0]} />
+            <filmPass args={[0.2, 0.5, 1500, false]} />
+        </Effects>
+    )
+
+}
