5d9385736bfc570139ded8f8887b4bece21a3e56
Denys Bondartsov <zimoby@gmail.com>
2024-02-16 18:01:10 +0200

    fix camera look and planet switching

--- src/components/ThreeJsMisc/index.tsx
+++ src/components/ThreeJsMisc/index.tsx
@@ -24,17 +24,19 @@ export const KeyboardInit = () => {
 
   const activeObjectName = useSystemStore.getState().activeObjectName;
 
+  const planetsWithSun = ["sun", ...planetsNamesOrder];
+
   // console.log("rightPressed", rightPressed, leftPressed, activeObjectName);
 
   useEffect(() => {
     if (!rightPressed && !leftPressed) { return; }
-    const findActivePlanet = planetsNamesOrder.findIndex((planetName) => planetName === activeObjectName);
+    const findActivePlanet = planetsWithSun.findIndex((planetName) => planetName === activeObjectName);
     if (rightPressed) {
-      updateActiveName(planetsNamesOrder[(findActivePlanet + 1) % planetsNamesOrder.length]);
+      updateActiveName(planetsWithSun[(findActivePlanet + 1) % planetsWithSun.length]);
     } else if (leftPressed) {
-      updateActiveName(planetsNamesOrder[(findActivePlanet - 1 + planetsNamesOrder.length) % planetsNamesOrder.length]);
+      updateActiveName(planetsWithSun[(findActivePlanet - 1 + planetsWithSun.length) % planetsWithSun.length]);
     }
-  }, [rightPressed, leftPressed, activeObjectName]);
+  }, [rightPressed, leftPressed, activeObjectName, planetsWithSun]);
 
   return <></>;
 }
@@ -43,78 +45,27 @@ export const ControlComponent = () => {
 
   const cameraDistance = 100;
 
-  // const refCamera = useRef();
+  const { camera } = useThree();
 
-  // const lookAtVec = (new THREE.Vector3(0, 0, 0));
-  // const cameraVector = (new THREE.Vector3(0, 0, 0));
-
-  // const activeObjectName = useSystemStore.getState().activeObjectName;
-  // const activeObjectNameAct = useMemo(() => activeObjectName, [activeObjectName]);
-
-  // const { camera } = useThree();
-
-  // console.log(camera)
-
-
-  // const [statesUpdate, setStatesUpdate] = useState(0);
-
-  // const activeObjectName = useSystemStore.getState().activeObjectName;
-  // const activeItemPositionRef = useRef(new THREE.Vector3(0, 0, 0));
-  // // const activeItemPositionReclearIntervalf = useRef({ needsUpdate: false });
-  // const activeItemPositionRec2 = useRef();
-
-  // console.log("activeObjectName", activeObjectName);
-
-
-  // useEffect(() => {
-  //   const unsubscribe = useSolarSystemStore.subscribe(
-  //     (state) => {
-        
-
-  //       const newPosition = state.properties[activeObjectNameAct]?.position;
-
-  //       if (!newPosition) { return; }
-  //       // lookAtVec.current.set(newPosition.x, newPosition.y, newPosition.z);
-  //       // cameraVector.current.lerp(lookAtVec.current, 0.1);
-  //       camera.lookAt(newPosition.x, newPosition.y, newPosition.z);
-  //       camera.updateProjectionMatrix();
-
-  //       // activeItemPositionRef.current = state.properties[activeObjectName]?.position;
-  //       // console.log("activeItemPositionRef", activeItemPositionRef.current);
-  //       // setStatesUpdate((prev) => prev + 1);
-  //     },
-  //     (state) => state.properties[activeObjectNameAct]
-  //   );
-  //   return unsubscribe;
-  // }, [activeObjectNameAct, camera, cameraVector]);
-
-
-  // useFrame((state) => {
-  //   const newPosition = useSolarSystemStore.getState().properties[activeObjectNameAct]?.position;
-  //   if (newPosition) {
-  //     refCamera.current.position.copy(newPosition);
-  //     refCamera.current.position.z += cameraDistance;
-  //   }
-  //   // if (!newPosition) { return; }
-    
-  //   // lookAtVec.set(newPosition.x, newPosition.y, newPosition.z);
-  //   // cameraVector.lerp(lookAtVec, 0.1);
-  //   // state.camera.lookAt(cameraVector);
-  //   // state.camera.updateProjectionMatrix();
-  // });
+  useFrame(() => {
+    const newPosition = useSolarSystemStore.getState().properties[useSystemStore.getState().activeObjectName]?.position;
+    if (newPosition) {
+      camera.lookAt(newPosition.x, newPosition.y, newPosition.z);
+    }
+  });
 
   return (
     <>
-      {/* <PerspectiveCamera  makeDefault position={[0, cameraDistance, cameraDistance * 2]} fov={10}  /> */}
+      <PerspectiveCamera  makeDefault position={[0, cameraDistance, cameraDistance * 2]} fov={10}  />
       <OrbitControls 
         // ref={activeItemPositionRec2}
-        // makeDefault
+        makeDefault
         // enablePan={true}
         // enableZoom={true}
         // enableDamping={true}
-        // dampingFactor={0.2}
-        // rotateSpeed={0.5}
-        // zoomSpeed={1}
+        dampingFactor={0.3}
+        rotateSpeed={0.5}
+
         // panSpeed={0.8}
         // target={activeItemPositionRef.current}
       />
