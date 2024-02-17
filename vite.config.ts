d647306888c276c53bc5d8da63f7b38d2cb986c5
Denys Bondartsov <zimoby@gmail.com>
2024-02-17 23:32:00 +0200

    add vite-plugin-glsl

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import glsl from 'vite-plugin-glsl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    glsl()
+  ],
 })
