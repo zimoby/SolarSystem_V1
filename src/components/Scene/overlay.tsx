ba29fd1f956e298b88f4a8212592d02489c73ec2
Denys Bondartsov <zimoby@gmail.com>
2024-02-17 12:14:27 +0200

    update ovarlay

--- src/components/Scene/overlay.tsx
+++ src/components/Scene/overlay.tsx
@@ -7,25 +7,27 @@ export const OverlayElements = () => {
                 <div className="flex flex-col">
                     <div className=" *:text-xs flex flex-row space-x-3 opacity-50  p-2 text-neutral-50">
                     <p>Three.js Journey</p>
-                    <p>:</p>
+                    <p>|</p>
                     <p>Challenge 008: Solar System</p>
-                    <p>:</p>
+                    <p>|</p>
                     <p>Author: Bondartsov Denys</p>
                 </div>
                 </div>
             </div>
             <div className="absolute top-0 left-0 select-none m-3 text-neutral-50">
-                <div className="flex flex-row space-x-4 divide-x-2 divide-white/50">
+                <div className="flex flex-row text-2xs space-x-4 divide-x-2 divide-white/50">
                     <div className="">
                         {svgLogo}
                     </div>
-                    <div className="flex flex-col">
-                        <div className="text-xs pl-4">
-                            {"Choose a planet with arrows ⟵ / ⟶"}
-                        </div>
-                        <div className="text-xs pl-4">
-                            {"Or click on the planet info"}
-                        </div>
+                    <div className="*:pl-4 flex flex-col">
+                        <p>Choose a planet with arrows ⟵ / ⟶</p>
+                        <p>Or click on the planet info</p>
+                    </div>
+                    <div className="*:pl-4 flex flex-col -space-y-1">
+                        <p>Planets size increased to x1200</p>
+                        <p>Sun size increased to x24</p>
+                        <p>Planets rotation decreased to x100</p>
+                        <p>Moons rotation decreased to x500</p>
                     </div>
                 </div>
             </div>
