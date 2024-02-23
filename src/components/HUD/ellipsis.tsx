27cd89111f24aa9614c355f969c009941f980cc7
Denys Bondartsov <zimoby@gmail.com>
2024-02-23 18:38:52 +0200

    add lines dissapearing

--- src/components/HUD/ellipsis.tsx
+++ src/components/HUD/ellipsis.tsx
@@ -1,11 +1,11 @@
-import { useMemo, useRef, useState } from "react";
-import { useSolarStore } from "../../store/systemStore";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { useSolarPositionsStore, useSolarStore } from "../../store/systemStore";
 import {
   degreesToRadians,
 } from "../../utils/calculations";
 import * as THREE from "three";
 import { Line, Ring } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 import { OrbitDisk } from "../Objects/disk";
 import { ObjectsAdditionalDataT } from "../../types";
 
@@ -21,7 +21,7 @@ export const ObjectEllipse: React.FC<ObjectEllipseProps> = ({
   params,
   name,
   color = "grey",
-  opacity = 1,
+  opacity = 0.75,
   type,
   extraRotation
 }) => {
@@ -32,10 +32,12 @@ export const ObjectEllipse: React.FC<ObjectEllipseProps> = ({
   const orbitAngleOffset = useSolarStore((state) => state.orbitAngleOffset);
   const planetsInitialized = useSolarStore((state) => state.planetsInitialized);
   const orbitPathDetalization = useSolarStore((state) => state.orbitPathDetalization);
+  const activeObject = useSolarStore((state) => state.activeObjectName);
 
-  console.log("ObjectEllipse", objData);
+  // console.log("ObjectEllipse", objData);
 
   const dashOffsetRef = useRef<THREE.Line>();
+  const orbitLineRef = useRef<THREE.Line>();
   // console.log("ObjectEllipse", params.orbitInclinationDeg);
   color = "white"
 
@@ -65,7 +67,7 @@ export const ObjectEllipse: React.FC<ObjectEllipseProps> = ({
     // return new THREE.RingGeometry(distanceXY.x, distanceXY.x + 0.1, 64, 1, 0, Math.PI * 2);
     return (
       <Ring
-        args={[(distanceXY.x - 1 * relScale), (distanceXY.y + 1 * relScale), 64, 1, 0, Math.PI * 2]}
+        args={[(distanceXY.x - 1 * relScale), (distanceXY.y + 1 * relScale), 128, 1, 0, Math.PI * 2]}
         position={[0, 0, 0]}
         rotation-x={Math.PI / 2}
         // color={color}
@@ -102,6 +104,41 @@ export const ObjectEllipse: React.FC<ObjectEllipseProps> = ({
     }
   }); 
 
+  const {camera} = useThree();
+
+  console.log("ObjectEllipse", name, 1+ useSolarStore.getState().additionalProperties[name]?.scale * 2);
+
+  useFrame(() => {
+    const objectPosition = useSolarPositionsStore.getState().properties[name]?.position as THREE.Vector3;
+    const activeScale = useSolarStore.getState().additionalProperties[name]?.scale;
+    const distance = camera.position.distanceTo(objectPosition);
+
+    // let opacity = 1;
+    const minDistance = 2 * (1 + activeScale * 5);
+    const maxDistance = 5 * (1 + activeScale * 5);
+
+    let distRelativeOpacity = 1;
+    if (distance <= maxDistance && distance >= minDistance) {
+      distRelativeOpacity = (distance - minDistance) / (maxDistance - minDistance);
+    } else if (distance < minDistance) {
+      distRelativeOpacity = 0;
+    }
+    const relativeOpacity = Math.min(Math.max(distRelativeOpacity, 0), 1) * (distRelativeOpacity);
+
+    if (dashOffsetRef.current && orbitLineRef.current) {
+      // console.log("opacity", dashOffsetRef.current.material);
+      dashOffsetRef.current.material.dashScale = (1 / distance) * 2000;
+
+      if (activeObject === name) {
+        dashOffsetRef.current.material.opacity = relativeOpacity / 2;
+        orbitLineRef.current.material.opacity = relativeOpacity;
+      } else {
+        dashOffsetRef.current.material.opacity = opacity / 2;
+        orbitLineRef.current.material.opacity = opacity;
+      }
+    }
+  });
+
   return (
     <group>
       <group rotation={[-extraRotation,0,0]} >
@@ -115,13 +152,14 @@ export const ObjectEllipse: React.FC<ObjectEllipseProps> = ({
         {/* <mesh rotation-x={Math.PI / 2 - extraRotation}> */}
           <Line
             // @ts-expect-error i'm tired of this
+            name="dashOffsetLine"
             ref={dashOffsetRef}
             position={[0, 0, 0]}
             points={pointsDependOnInclination}
             color={!selected ? color : "yellow"}
             lineWidth={1}
             transparent={true}
-            opacity={opacity / 2}
+            // opacity={opacity / 2}
             dashed={true}
             dashSize={5}
             dashScale={30}
@@ -129,15 +167,16 @@ export const ObjectEllipse: React.FC<ObjectEllipseProps> = ({
         </mesh>
         <mesh rotation-x={Math.PI / 2 + planetInclination}>
           <Line
+            ref={orbitLineRef}
             position={[0, 0, 0]} // (params.anchorXYOffset?.y ?? 0)
             points={points}
             color={!selected ? color : "yellow"}
             lineWidth={!selected ? 1 : 2}
             transparent={true}
-            opacity={opacity}
-            dashed={type === "moons"}
-            dashSize={0.3}
-            dashScale={10}
+            // opacity={opacity}
+            // dashed={type === "moons"}
+            // dashSize={0.3}
+            // dashScale={10}
           />
         </mesh>
       </group>
