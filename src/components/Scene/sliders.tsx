9af4189fa8cd715bd4f6ee20eae52d5bbf5df74d
Denys Bondartsov <zimoby@gmail.com>
2024-02-25 23:27:52 +0200

    add new custom controls

--- src/components/Scene/sliders.tsx
+++ src/components/Scene/sliders.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSolarStore } from "../../store/systemStore";
 
 import "./styles.css";
@@ -17,6 +17,31 @@ type SliderWithInputProps = {
     onUpdate: (value: number) => void;
 };
 
+type CheckBoxProps = {
+    label: string;
+    value: boolean;
+    onUpdate: (value: boolean) => void;
+};
+
+const CheckBox = ({ label, value, onUpdate }: CheckBoxProps) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        onUpdate(e.target.checked);
+    };
+
+    return (
+        <div className="relative flex flex-row text-xs justify-start items-center">
+            <div className="relative w-32 truncate select-none">
+                {label}
+            </div>
+            <input
+                type="checkbox"
+                checked={value}
+                onChange={handleChange}
+            />
+        </div>
+    );
+}
+
 const SliderWithInput = ({ label, value, min, max, step, onUpdate }: SliderWithInputProps) => {
     const [localValue, setLocalValue] = useState<number>(value);
 
@@ -24,13 +49,22 @@ const SliderWithInput = ({ label, value, min, max, step, onUpdate }: SliderWithI
         setLocalValue(Number(e.target.value));
     };
 
-    const handleMouseUp = () => {
-        onUpdate(localValue);
+    const allChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setLocalValue(Number(e.target.value));
+        onUpdate(Number(e.target.value));
+    }
+
+    const handleMouseUp = (e: React.MouseEvent<HTMLInputElement>) => {
+        onUpdate(Number(e.currentTarget.value));
     };
+
+    useEffect(() => {
+        setLocalValue(value);
+    }, [value]);
 
     return (
         <div className="relative flex flex-row text-xs justify-center items-center">
-            <div className="relative flex w-20 truncate select-none">
+            <div className="relative w-20 truncate select-none">
                 {label}
             </div>
             <input
@@ -47,13 +81,13 @@ const SliderWithInput = ({ label, value, min, max, step, onUpdate }: SliderWithI
                 onMouseUp={handleMouseUp}
             />
             <input
-                className="h-hull m-0 w-10 rounded-sm border-transparent bg-black/20 p-0 text-center text-xs text-white"
+                className="h-hull ml-2 m-0 w-10 rounded-sm border-transparent bg-black/20 p-0 text-center text-xs text-white"
                 min={min.toString()}
                 max={max.toString()}
                 step={step.toString()}
                 type="number"
                 value={localValue}
-                onChange={handleChange}
+                onChange={allChange}
             />
         </div>
     );
@@ -63,25 +97,83 @@ export const SolarSystemControls = () => {
     const updateSystemSettings = useSolarStore((state) => state.updateSystemSettings);
 
     // const disableMoons = useSolarStore((state) => state.disableMoons);
-    // const disableTrash = useSolarStore((state) => state.disableTrash);
+    const disableTrash = useSolarStore((state) => state.disableTrash);
+    const disableRandomObjects = useSolarStore((state) => state.disableRandomObjects);
     // const disableRandomObjects = useSolarStore((state) => state.disableRandomObjects);
-    const timeSpeed = useSolarStore((state) => state.timeSpeed / 100000) ;
-    // const timeOffset = useSolarStore((state) => state.timeOffset);
+    const timeSpeed = useSolarStore((state) => state.timeSpeed);
+    const timeOffset = useSolarStore((state) => state.timeOffset);
+    const objectsDistance = useSolarStore((state) => state.objectsDistance);
+    const objectsRelativeScale = useSolarStore((state) => state.objectsRelativeScale);
+    const orbitAngleOffset = useSolarStore((state) => state.orbitAngleOffset);
 
-    const handleSliderUpdate = (value: number, param: string) => {
-        updateSystemSettings({ [param]: value * 100000 });
+//     objectsDistance
+// objectsRelativeScale
+// orbitAngleOffset
+
+    const handleSliderUpdate = (value: number | boolean, param: string, correction: number = 1) => {
+        updateSystemSettings({ [param]: Number(value) * correction });
     };
 
     return (
         <div className="absolute z-50 top-0 right-0 m-2 flex flex-col space-y-0">
-            <SliderWithInput 
-                label="Time speed"
-                value={timeSpeed}
-                min={0}
-                max={100}
-                step={1}
-                onUpdate={(e) => handleSliderUpdate(e, "timeSpeed")}
-            />
+            <div className=" space-y-1 bg-black/20 py-1 px-2 divide-y divide-white/40 rounded-md">
+                <div className=" space-y-1 ">
+                    <SliderWithInput 
+                        label="Time Speed"
+                        value={timeSpeed / 100000}
+                        min={0}
+                        max={100}
+                        step={1}
+                        onUpdate={(e) => handleSliderUpdate(e, "timeSpeed", 100000)}
+                    />
+                    <SliderWithInput 
+                        label="Time Offset"
+                        value={timeOffset}
+                        min={-360}
+                        max={360}
+                        step={1}
+                        onUpdate={(e) => handleSliderUpdate(e, "timeOffset")}
+                    />
+                </div>
+                <div className=" space-y-1  pt-1">
+                    <SliderWithInput 
+                        label="Relative Distance"
+                        value={objectsDistance}
+                        min={1}
+                        max={5}
+                        step={1}
+                        onUpdate={(e) => handleSliderUpdate(e, "objectsDistance")}
+                    />
+                    <SliderWithInput 
+                        label="Relative Scale"
+                        value={objectsRelativeScale}
+                        min={1}
+                        max={10}
+                        step={1}
+                        onUpdate={(e) => handleSliderUpdate(e, "objectsRelativeScale")}
+                    />
+                    <SliderWithInput 
+                        label="Orbit Rotation"
+                        value={orbitAngleOffset}
+                        min={0}
+                        max={360}
+                        step={1}
+                        onUpdate={(e) => handleSliderUpdate(e, "orbitAngleOffset")}
+                    />
+                </div>
+                <div className=" space-y-1 pt-1">
+                    <CheckBox
+                        label="Disable Asteroids"
+                        value={disableTrash}
+                        onUpdate={() => handleSliderUpdate(!disableTrash, "disableTrash")}
+                    />
+                    <CheckBox
+                        label="Disable Objects"
+                        value={disableRandomObjects}
+                        onUpdate={() => handleSliderUpdate(!disableRandomObjects, "disableRandomObjects")}
+                    />
+                </div>
+            </div>
         </div>
     );
 }
