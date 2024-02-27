fb71b235e54ec04fc0af0a87a13edbd68145e196
Denys Bondartsov <zimoby@gmail.com>
2024-02-28 00:52:04 +0200

    test camera fix

--- src/components/ThreeJsMisc/index.tsx
+++ src/components/ThreeJsMisc/index.tsx
@@ -5,6 +5,7 @@ import { Controls } from "../../types";
 import { planetsNamesOrder } from "../../data/solarSystemData";
 import { useFrame, useThree } from "@react-three/fiber";
 import { updateActiveName } from "../../hooks/storeProcessing";
+import { Vector3 } from "three";
 
 export const AppStatsPerformance = () => {
   return (
@@ -52,7 +53,7 @@ export const ControlComponent = () => {
     const unsubscribeSolarPositions = useSolarPositionsStore.subscribe((state) => {
       const newPosition = state.properties[useSolarStore.getState().activeObjectName]?.position;
       // console.log("ControlComponent", {newPosition});
-      currentPositionRef.current = newPosition as THREE.Vector3 | undefined;
+      currentPositionRef.current = newPosition as Vector3 | undefined;
     });
 
     return () => {
@@ -63,7 +64,7 @@ export const ControlComponent = () => {
   useEffect(() => {
     // console.log("ControlComponent", {currentObjectName, additionalData});
     if (currentObjectName !== "sun" && currentPositionRef.current) {
-      const newPosition = useSolarPositionsStore.getState().properties[currentObjectName]?.position as THREE.Vector3;
+      const newPosition = useSolarPositionsStore.getState().properties[currentObjectName]?.position as Vector3;
       const scale = useSolarStore.getState().additionalProperties[currentObjectName].scale || 1;
       // console.log("ControlComponent", {currentObjectName, additionalData}, newPosition); 
       camera.position.set(newPosition.x + 30 * scale, newPosition.y + 2 * scale, newPosition.z + 30 * scale);
@@ -72,31 +73,11 @@ export const ControlComponent = () => {
 
 
   useFrame(() => {
-    if (currentPositionRef.current) {
-      // const desiredOffset = new Vector3(0, 10, -30);
-      // const targetPosition = currentPositionRef.current;
-      // const desiredPosition = targetPosition.clone().add(desiredOffset);
-      // camera.position.lerp(desiredPosition, 0.1);
-      // camera.lookAt(targetPosition);
-
-      
-      // const targetPosition = currentPositionRef.current;
-      // const controls = orbitControlsRef.current;
-      // controls.target.set(targetPosition.x, targetPosition.y, targetPosition.z);
-      // // controls.position0.set(targetPosition.x, targetPosition.y,  targetPosition.z);
-      // controls.update();
-
-      // console.log(camera.zoom)
-
-      // camera.zoom = currentObjectName !== "sun" ? 2 / (additionalData.scale || 1) : 1;
-
-      // camera.position.lerp(desiredPosition, 0.1)
-      // camera.position.set(currentPositionRef.current.x, currentPositionRef.current.y, currentPositionRef.current.z + cameraDistance);
-      camera.lookAt(currentPositionRef.current.x, currentPositionRef.current.y, currentPositionRef.current.z);
-      // camera.lookAt(targetPosition);
-
-      // camera.position.set(currentPositionRef.current.x, currentPositionRef.current.y, currentPositionRef.current.z + cameraDistance);
-      // camera.position.set(currentPositionRef.current.x + cameraPosition.x, currentPositionRef.current.y + cameraPosition.y, currentPositionRef.current.z + cameraPosition.z);
+    if (currentPositionRef.current && orbitControlsRef.current) {
+      const controls = orbitControlsRef.current;
+      // console.log("ControlComponent", {controls});
+      // @ts-expect-error tyred of typescript
+      controls.target.set(currentPositionRef.current.x, currentPositionRef.current.y, currentPositionRef.current.z);
     }
   });
 
@@ -107,6 +88,7 @@ export const ControlComponent = () => {
         ref={orbitControlsRef}
         dampingFactor={0.3}
         rotateSpeed={0.5}
+        enablePan={true}
       />
     </>
   );
